3b0f3e1489bd1a9501c5636cf3fc8cf3fd5675f3
diwei.deng <diwei@deng.com>
2022-02-11 16:51:51 +0800

    hot-fix 1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,8 +2,8 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("test...git!!!");
-        System.out.println("test...git2!!!");
-        System.out.println("test...git3!!!");
+        System.out.println("test...git4!!!");
+        System.out.println("test...git5!!!");
+        System.out.println("test...git6!!!");
     }
 }

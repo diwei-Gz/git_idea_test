0acb1764a61879c796ba5f1786204f84f669eda1
diwei.deng <diwei@deng.com>
2022-02-11 16:56:40 +0800

    master 2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,8 +2,9 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("test...git4!!!");
-        System.out.println("test...git5!!!");
-        System.out.println("test...git6!!!");
+        System.out.println("test...git!!!");
+        System.out.println("test...git!!!");
+        System.out.println("test...git!!!");
+        System.out.println("1111111");
     }
 }

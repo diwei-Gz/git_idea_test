63622900b40e4ae35c9da2965bd1d44dab108c3f
diwei.deng <diwei@deng.com>
2022-02-11 16:55:02 +0800

    hot-fix 1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("test...git4!!!");
         System.out.println("test...git5!!!");
-        System.out.println("test...git6!!!");
+        System.out.println("test...git!!!");
+        System.out.println("11111");
     }
 }

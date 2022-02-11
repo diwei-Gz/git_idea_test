eb432895b4d217518a8608b4e0baefe0756b656a
diwei-Gz <95429746+diwei-Gz@users.noreply.github.com>
2022-02-11 18:42:24 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("test master");
         System.out.println("test hot-fix");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
